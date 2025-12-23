a49246d1b849f709d1a2936865e0b085b06c0d8d
Cristóbal Mundaca <cristobal.mundacav@gmail.com>
2025-12-23 16:34:43 -0300

    Proyecto React : Agregacion de users.map() para la transformacion de estos en componentes con tw-followCard

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,46 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App(){
 
+    const users=[
+        {
+            userName:'midudev',
+            name:'Miguel Angel Duran',
+            isFollowing: true
+        },
+        {
+            userName:'pheralb',
+            name:'Pablo Hernandez',
+            isFollowing: false            
+        },
+        {
+            userName:'Elon Musk',
+            name:'Elon Musk',
+            isFollowing: false
+        }
+        ,
+        {
+            userName:'unknown',
+            name:'Usuario Desconocido',
+            isFollowing: true
+        }
+    ]
+
     return(
         <section className='App'>
+            {
+                users.map(user => {
+                    const {userName,name,isFollowing}=user
+                    return(
+                        <TwitterFollowCard
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}>
+                            {name}
+                        </TwitterFollowCard>
 
-            <TwitterFollowCard userName='midudev' initialIsFollowing={true} >
-                Miguel Angel Duran
-            </TwitterFollowCard>
-
-            <TwitterFollowCard userName="pheralb">
-                Pablo Hernandez
-            </TwitterFollowCard>
+                    )
+                })
+            }
         </section>
         )
 }
